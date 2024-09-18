2b4bf5dd98196913cd7e2ce317bb5652b904ddc5
roi_louis_ix <matthewjosephbarry@outlook.com>
2024-09-17 21:05:27 -0400

    :3

--- lab4_p1/main.c
+++ lab4_p1/main.c
@@ -3,14 +3,16 @@
 //*****************************************************************************
 //  DESIGNER NAME:  Matthew Barry
 //
-//       LAB NAME:  Lab 4 Part 1: Interfacing with LEDs
+//       LAB NAME:  Lab 4: Interfacing with LEDs
 //
 //      FILE NAME:  main.c
 //
 //-----------------------------------------------------------------------------
 //
 // DESCRIPTION:
-//    This program serves as a ... 
+//    This program is to help control pins on the LaunchPad board and the
+//    expansion board. It controls the expansion board LED bar, the LaunchPad
+//    LED bar, and the Launchpad 7-segment display.
 //
 //*****************************************************************************
 //*****************************************************************************
@@ -34,106 +36,132 @@
 //-----------------------------------------------------------------------------
 // Define function prototypes used by the program
 //-----------------------------------------------------------------------------
-
+void run_lab4_part4(void);
+void run_lab4_part5(void);
+void run_lab4_part6(void);
 
 //-----------------------------------------------------------------------------
 // Define symbolic constants used by the program
 //-----------------------------------------------------------------------------
-//Define the configuration data for the LEDs on the CSC202 Board
-const gpio_struct led_config_data[] = {
-    {LED0_PORT, LED0_MASK, LED0_IOMUX, LED0_LOW},
-    {LED1_PORT, LED1_MASK, LED1_IOMUX, LED1_LOW},
-    {LED2_PORT, LED2_MASK, LED2_IOMUX, LED2_LOW},
-    {LED3_PORT, LED3_MASK, LED3_IOMUX, LED3_LOW},
-    {LED4_PORT, LED4_MASK, LED4_IOMUX, LED4_LOW},
-    {LED5_PORT, LED5_MASK, LED5_IOMUX, LED5_LOW},
-    {LED6_PORT, LED6_MASK, LED6_IOMUX, LED6_LOW},
-    {LED7_PORT, LED7_MASK, LED7_IOMUX, LED7_LOW},
-};
+#define inbetween       (500)
 
-GPIOA->GPRCM.RSTCTL = (GPIO_RSTCLT_KEY_UNLOCK_W |
-                       GPIO_RSTCTL_RESETSTKYCLR_CLR |
-                       GPIO_RSTCTL_RESETASSERT_ASSERT);
+#define int_leds_on     (60)
+#define p1delay         (2000)
 
-GPIOB->GPRCM.RSTCTL = (GPIO_RSTCLT_KEY_UNLOCK_W |
-                       GPIO_RSTCTL_RESETSTKYCLR_CLR |
-                       GPIO_RSTCTL_RESETASSERT_ASSERT);
+#define p2delay         (200)
 
-GPIOA->GPRCM.PWREN = (GPIO_PWREN_KEY_UNLOCK_W | GPIO_PWREN_ENABLE_ENABLE);
-GPIOB->GPRCM.PWREN = (GPIO_PWREN_KEY_UNLOCK_W | GPIO_PWREN_ENABLE_ENABLE);
+#define p3delay         (250)
 
-IOMUX->SECCFG.PINCM[IOMUX_PINCM3]  = IOMUX_PINCM_PC_CONNECTED |
-                                     IOMUX_PINCM1_PF_GPIOA_DIO28;
+#define seg7L           (56)
+#define p4seg7          (0)
+#define p4delay         (2000)
 
-IOMUX->SECCFG.PINCM[IOMUX_PINCM6]  = IOMUX_PINCM_PC_CONNECTED |
-                                     IOMUX_PINCM57_PF_GPIOA_DIO31;
+#define p5on            (2000)
+#define p5off           (3000)
+#define p5seg7          (2)
+#define seg74           (102)
 
-IOMUX->SECCFG.PINCM[IOMUX_PINCM42] = IOMUX_PINCM_PC_CONNECTED |
-                                     IOMUX_PINCM58_PF_GPIOA_DIO20;
-
-IOMUX->SECCFG.PINCM[IOMUX_PINCM30] = IOMUX_PINCM_PC_CONNECTED |
-                                     IOMUX_PINCM50_PF_GPIOB_DIO13;
-
-IOMUX->SECCFG.PINCM[IOMUX_PINCM34] = IOMUX_PINCM_PC_CONNECTED |
-                                     IOMUX_PINCM1_PF_GPIOA_DIO12;
-
-IOMUX->SECCFG.PINCM[IOMUX_PINCM35] = IOMUX_PINCM_PC_CONNECTED |
-                                     IOMUX_PINCM1_PF_GPIOA_DIO13;
-
-IOMUX->SECCFG.PINCM[IOMUX_PINCM38] = IOMUX_PINCM_PC_CONNECTED |
-                                     IOMUX_PINCM1_PF_GPIOA_DIO16;
-
-IOMUX->SECCFG.PINCM[IOMUX_PINCM39] = IOMUX_PINCM_PC_CONNECTED |
-                                     IOMUX_PINCM1_PF_GPIOA_DIO17;
+#define p6delay         (1)
+#define seg7c           (57)
+#define seg7a           (119)
+#define seg7f           (113)
+#define seg7e           (121)
 
 
-//-----------------------------------------------------------------------------
-// Define global variables and structures here.
-// NOTE: when possible avoid using global variables
-//-----------------------------------------------------------------------------
-
-
-// Define a structure to hold different data types
-
-typedef struct {
-    uint8_t     port_id;
-    uint32_t    bit_mask;
-    uint16_t    pin_cm;
-    uint8_t     polarity;
-}
 
 int main(void)
 {
-  GPIOA->DOUT31_0 |= GPIO_DOUT31_0_DIO20_MASK | GPIO_DOUT31_0_DIO13_MASK | GPIO_DOUT31_0_DIO12_MASK;
-  GPIOB->DOUT31_0 |= GPIO_DOUT31_0_DIO13_MASK;
-  for(uint8_1 loop_cntr = 0; loop_cntr < 10; loop_cntr++)
-  {
-    GPIOA->DOUT31_0 |= GPIO_DOUT31_0_DIO28_MASK;
-    msec_delay(200);
-    GPIOA->DOUT31_0 &= ~GPIO_DOUT31_0_DIO28_MASK;
-    GPIOA->DOUT31_0 |= GPIO_DOUT31_0_DIO31_MASK;
-    msec_delay(200);
-    GPIOA->DOUT31_0 &= ~GPIO_DOUT31_0_DIO31_MASK;
-    GPIOA->DOUT31_0 |= GPIO_DOUT31_0_DIO20_MASK;
-    msec_delay(200);
-    GPIOA->DOUT31_0 &= ~GPIO_DOUT31_0_DIO20_MASK;
-    GPIOB->DOUT31_0 |= GPIO_DOUT31_0_DIO13_MASK;
-    msec_delay(200);
-    GPIOB->DOUT31_0 &= ~GPIO_DOUT31_0_DIO13_MASK;
-    GPIOA->DOUT31_0 |= GPIO_DOUT31_0_DIO12_MASK;
-    msec_delay(200);
-    GPIOA->DOUT31_0 &= ~GPIO_DOUT31_0_DIO12_MASK;
-    GPIOA->DOUT31_0 |= GPIO_DOUT31_0_DIO13_MASK;
-    msec_delay(200);
-    GPIO->DOUT31_0 &= ~GPIO_DOUT31_0_DIO13_MASK;
-    GPIOA->DOUT31_0 |= GPIO_DOUT31_0_DIO16_MASK;
-    msec_delay(200);
-    GPIO->DOUT31_0 &= ~GPIO_DOUT31_0_DIO16_MASK;
-    GPIOA->DOUT31_0 |= GPIO_DOUT31_0_DIO17_MASK;
-    msec_delay(200);
-    GPIO->DOUT31_0 &= ~GPIO_DOUT31_0_DIO17_MASK;
-  }
+    clock_init();
+    led_init();
+    led_enable();
+    seg7_init();
 
-  
+    leds_on(int_leds_on);
+    msec_delay(p1length);
+    leds_off(int_leds_on);
+
+    msec_delay(inbetween);
+
+    while(i < 5, i = 0, i++)
+    {
+        for(j = 0, j < 8, j++)
+        {
+            led_on(led_config_data[j]);
+            msec_delay(p2delay);
+            led_off(led_config_data[j]);
+        }
+
+        for(k = 0, k < 6, k--)
+        {
+            led_on(led_config_data[k]);
+            msec_delay(p2delay);
+            led_off(led_config_data[k]);
+        }
+        
+    }
+
+    msec_delay(inbetween);
+
+    while(i < 2, i=0, i++)
+    {
+        for(j = 0, j < 256, j++)
+        {
+            leds_on(led_config_data[j]);
+            msec_delay(p3delay);
+            leds_off(led_config_data[j]);
+        }
+    }
+
+    msec_delay(inbetween);
+
+    run_lab4_part4();
+
+    msec_delay(inbetween);
+
+    run_lab4_part5();
+
+    msec_delay(inbetween);
+
+
+
 } /* main */
 
+void run_lab4_part4()
+{
+    seg7_on(seg7L, p4seg7);
+    msec_delay(delay);
+    seg7_off(seg7L, p4seg7);
+}
+
+void run_lab4_part5()
+{
+    while(i < 4, i = 0, i++)
+    {
+        seg7_on(seg74, p5seg7);
+        msec_delay(p5on);
+        seg7_off(seg74, p5off);
+    }
+    
+}
+
+void run_lab4_part6()
+{
+    while(i < 200, i = 0, i++)
+    {
+        seg7_on(seg7c, 0);
+        msec_delay(p6delay);
+        seg7_off(seg7c, 0);
+
+        seg7_on(seg7a, 1);
+        msec_delay(p6delay);
+        seg7_off(seg7_a, 1);
+
+        seg7_on(seg7f, 2);
+        msec_delay(p6delay);
+        seg7_off(seg7f);
+
+        seg7_on(seg7e, 3);
+        msec_delay(p6delay);
+        seg7_off(seg7e, e);
+    }
+}
