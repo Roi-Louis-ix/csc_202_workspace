373d03127b85a993a6a29155c93565e91463c79d
roi_louis_ix <matthewjosephbarry@outlook.com>
2024-09-19 21:36:20 -0400

    :3

--- lab4_p1/main.c
+++ lab4_p1/main.c
@@ -48,30 +48,27 @@ void run_lab4_part6();
 //-----------------------------------------------------------------------------
 // Define symbolic constants used by the program
 //-----------------------------------------------------------------------------
-#define In_Between      (500)
+#define In_Between       (500)    //Time between parts
 
-#define Int_LEDs_On     (60)
-#define P1_Delay        (2000)
-#define numofLEDs       (8)
+#define Int_LEDs_On      (60)     //Corresponds to bits 2-5 being active
+#define P1_Delay         (2000)   //Length of Part 1 in ms
 
-#define p2delay         (50)
+#define P2_Delay         (200)    //Time of each LED in Part 2 in ms
 
-#define p3delay         (50)
+#define P3_Delay         (250)    //Time of each LED in Part 3 in ms
 
-#define seg7L           (56)
-#define p4seg7          (0)
-#define p4delay         (2000)
+#define Seg7_L           (56)     //Corresponds to the 7-segment code for L
+#define P4_Delay         (2000)   //Length of Part 4 in ms
 
-#define p5on            (3000)
-#define p5off           (2000)
-#define p5seg7          (2)
-#define seg74           (102)
+#define P5_On            (3000)   //7seg display on time in ms
+#define P5_Off           (2000)   //7seg display off time in ms
+#define Seg7_4           (102)    //Corresponds to the 7-segment code for 4
 
-#define p6delay         (1)
-#define seg7c           (57)
-#define seg7a           (119)
-#define seg7f           (113)
-#define seg7e           (121)
+#define P6_Delay         (1)      //Time of each 7seg display in Part 6
+#define Seg7_C           (57)     //Corresponds to the 7-segment code for C,
+#define Seg7_A           (119)    //A,
+#define Seg7_F           (113)    //F,
+#define Seg7_E           (121)    //E
 
 
 
@@ -98,6 +95,21 @@ int main(void)
 
 } /* main */
 
+
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+//  This function activates bits 2-5 based on Int_LEDs_On for a length of 
+//  P1_Delay milliseconds.
+//  
+// INPUT PARAMETERS:
+// none
+//
+// OUTPUT PARAMETERS:
+//  none
+//
+// RETURN:
+// none
+// -----------------------------------------------------------------------------
 void run_lab4_part1()
 {
     leds_on(Int_LEDs_On);
@@ -106,6 +118,22 @@ void run_lab4_part1()
 
     msec_delay(In_Between);
 }
+
+
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+// This function activates LEDs 0 through 7 10 times. 
+//  
+//  
+// INPUT PARAMETERS:
+// none
+//
+// OUTPUT PARAMETERS:
+//  none
+//
+// RETURN:
+// none
+// -----------------------------------------------------------------------------
 void run_lab4_part2()
 {
     uint8_t loop_count = 0;
@@ -115,20 +143,28 @@ void run_lab4_part2()
         uint8_t countup_index = 0;
         uint8_t start_countup_loop = 0;
         uint8_t end_countup_loop = 8;
-        for(countup_index = start_countup_loop; countup_index < end_countup_loop; countup_index++)
+        for(
+            countup_index = start_countup_loop;
+            countup_index < end_countup_loop; 
+            countup_index++
+            )
         {
             led_on(countup_index);
-            msec_delay(p2delay);
+            msec_delay(P2_Delay);
             led_off(countup_index);
         }
 
         uint8_t countdown_index = 0;
         uint8_t start_countdown_loop = 6;
         uint8_t end_countdown_loop = 0;
-        for(countdown_index = start_countdown_loop; countdown_index > end_countdown_loop; countdown_index--)
+        for(
+            countdown_index = start_countdown_loop; 
+            countdown_index > end_countdown_loop; 
+            countdown_index--
+            )
         {
             led_on(countdown_index);
-            msec_delay(p2delay);
+            msec_delay(P2_Delay);
             led_off(countdown_index);
         }
         
@@ -139,6 +175,21 @@ void run_lab4_part2()
     msec_delay(In_Between);
 }
 
+
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+// This function displays a binary counter from 0-255 on LEDs 0 through 7. 
+//  
+//  
+// INPUT PARAMETERS:
+// none
+//
+// OUTPUT PARAMETERS:
+//  none
+//
+// RETURN:
+// none
+// -----------------------------------------------------------------------------
 void run_lab4_part3()
 {
     uint8_t loop_count = 0;
@@ -153,7 +204,7 @@ void run_lab4_part3()
         for(countup_index = start_countup_loop; countup_index < end_countup_loop; countup_index++)
         {
             leds_on(countup_index);
-            msec_delay(p3delay);
+            msec_delay(P3_Delayelay);
             leds_off();
         }
         loop_count++;
@@ -163,17 +214,49 @@ void run_lab4_part3()
 
 }
 
+
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+// This function displays the letter L on 7seg display 0 for a period of 
+// P4_Delay ms.
+//  
+//  
+// INPUT PARAMETERS:
+// none
+//
+// OUTPUT PARAMETERS:
+//  none
+//
+// RETURN:
+// none
+// -----------------------------------------------------------------------------
 void run_lab4_part4()
 {
     led_disable();
 
-    seg7_on(seg7L, SEG7_DIG0_ENABLE_IDX);
-    msec_delay(p4delay);
+    seg7_on(Seg7_L, SEG7_DIG0_ENABLE_IDX);
+    msec_delay(P4_Delay);
     seg7_off();
 
     msec_delay(In_Between);
 }
 
+
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+// This function flashes the number 4 on 7seg display 4 times; on for a period
+// of P5_On ms and off for a period of P5_Off ms.  
+//  
+//  
+// INPUT PARAMETERS:
+// none
+//
+// OUTPUT PARAMETERS:
+//  none
+//
+// RETURN:
+// none
+// -----------------------------------------------------------------------------
 void run_lab4_part5()
 {
     uint8_t loop_count = 0;
@@ -181,10 +264,10 @@ void run_lab4_part5()
     
     while(loop_count < p5iterations)
     {
-        seg7_on(seg74, SEG7_DIG2_ENABLE_IDX);
-        msec_delay(p5on);
+        seg7_on(Seg7_4, SEG7_DIG2_ENABLE_IDX);
+        msec_delay(P5_On);
         seg7_off();
-        msec_delay(p5off);
+        msec_delay(P5_Off);
 
         loop_count++;
     }
@@ -193,26 +276,42 @@ void run_lab4_part5()
 }
 
 
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+// This function multiplexes between 7seg displays 0 and 3 to display the word
+// "CAFE". It cycles between the displays 200 times with each letter being
+// displayed for 1 ms. 
+//  
+//  
+// INPUT PARAMETERS:
+// none
+//
+// OUTPUT PARAMETERS:
+//  none
+//
+// RETURN:
+// none
+// -----------------------------------------------------------------------------
 void run_lab4_part6()
 {
     uint8_t loop_count = 0;
     uint8_t p6iterations = 200;
     while(loop_count < p6iterations)
     {
-        seg7_on(seg7c, SEG7_DIG0_ENABLE_IDX);
-        msec_delay(p6delay);
+        seg7_on(Seg7_C, SEG7_DIG0_ENABLE_IDX);
+        msec_delay(P6_Delay);
         seg7_off();
 
-        seg7_on(seg7a, SEG7_DIG1_ENABLE_IDX);
-        msec_delay(p6delay);
+        seg7_on(Seg7_A, SEG7_DIG1_ENABLE_IDX);
+        msec_delay(P6_Delay);
         seg7_off();
 
-        seg7_on(seg7f, SEG7_DIG2_ENABLE_IDX);
-        msec_delay(p6delay);
+        seg7_on(Seg7_F, SEG7_DIG2_ENABLE_IDX);
+        msec_delay(P6_Delay);
         seg7_off();
 
-        seg7_on(seg7e, SEG7_DIG3_ENABLE_IDX);
-        msec_delay(p6delay);
+        seg7_on(Seg7_E, SEG7_DIG3_ENABLE_IDX);
+        msec_delay(P6_Delay);
         seg7_off();
 
         loop_count += 1;
