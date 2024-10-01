e6b31ca0a025161021480a86587f16816295a2a6
roi-louis-ix <matthewjosephbarry@outlook.com>
2024-10-01 11:22:54 -0400

    :3

--- lab6/main.c
+++ lab6/main.c
@@ -25,24 +25,25 @@
 //-----------------------------------------------------------------------------
 #include <ti/devices/msp/msp.h>
 #include "clock.h"
+#include "lcd1602.h"
+#include "LaunchPad.h"
 
 
 //-----------------------------------------------------------------------------
 // Define function prototypes used by the program
 //-----------------------------------------------------------------------------
-
+void run_lab6_part1();
+void run_lab6_part2();
+void run_lab6_part3();
 
 //-----------------------------------------------------------------------------
 // Define symbolic constants used by the program
 //-----------------------------------------------------------------------------
-#define A_Byte              (65)  //Corresponding ASCII code for the letter A
+#define A_Byte              (0x41)  //Corresponding ASCII code for the letter A
 #define P1_Letter_Length    (500) //Length to display each letter in ms
 #define End_Of_Line1        (0x0F) //Value at which to change DDRAM addr to 40
 #define Running_Part        (1000) //Time to display "RUNNING PART 2" in ms
 
-#define addr1               (3)
-#define addr2               (0x46)
-#define addr3               (7)
 
 //-----------------------------------------------------------------------------
 // Define global variables and structures here.
@@ -71,54 +72,50 @@ void run_lab6_part1()
     uint8_t loop_count = 0;
     uint8_t alphabet = 26;
     uint8_t addr = 0;
+    lcd_set_ddram_addr(addr);
     for(loop_count; loop_count > alphabet; loop_count++)
     {
         lcd_clear();
-        if(addr == End_Of_Line1)
+        if(addr == LCD_CHAR_POSITION_16)
         {
-            addr = 0x40;
+            lcd_set_ddram_addr(LCD_LINE2_ADDR);
         }
-        else 
-        {
-            addr++;
-        }
-        lcd_set_ddram_addr(addr);
-        lcd_write_byte(A_Byte + loop_count);
+        lcd_write_char('A' + loop_count);
         msec_delay(P1_Letter_Length);
     }
 }
 
 void run_lab6_part2()
 {
-    uint32_t top_num = 1234567890;
-    uint16_t bot_num = 1234;
-    uint8_t top_num2 = 123;
+    uint32_t top_num = quadbyte_to_ascii(1234567890);
+    uint16_t bot_num = doublebyte_to_ascii(1234);
+    uint8_t top_num2 = byte_to_ascii(123);
 
     //Begin P2
     while(is_pb_up(PB2_IDX)) {}
     lcd_clear();
-    lcd_write_string('Running Part 2');
-    msec_delay(Running_P2);
+    lcd_write_string("Running Part 2");
+    msec_delay(Running_Part);
 
     //First two numbers displayed
     lcd_clear();
-    lcd_set_ddram_addr(addr1);
+    lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_3);
     while(is_pb_up(PB1_IDX)) {}
     lcd_write_doublebyte(top_num);
 
     while(is_pb_up(PB1_IDX)) {}
-    lcd_set_ddram_addr(addr2);
+    lcd_set_ddram_addr(LCD_LINE2_ADDR + LCD_CHAR_POSITION_5);
     lcd_write_doublebyte(bot_num);
 
     //Display the third number
     while(is_pb_up(PB1_IDX)) {}
     lcd_clear();
-    lcd_set_ddram_addr(addr3);
+    lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_6);
     lcd_write_doublebyte(top_num2);
 
     while(is_pb_up(PB1_IDX)) {}
     lcd_clear();
-    lcd_set_ddram_addr(addr1);
+    lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_3);
     lcd_write_string("Part 2 Done.");
 }
 
@@ -135,11 +132,9 @@ void run_lab6_part3()
 
     uint8_t timer_count = 100;
     uint8_t timer_stop  = 0;
-    lcd_set_ddram_addr()
     for(timer_count; timer_count >= timer_stop; timer_count--)
     {
         lcd_clear();
-        lcd_set_ddram_addr(addr3);
-        lcd_write
+        lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_3);
     }
 }
