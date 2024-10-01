4ebec7affc3d164e09d6f0c0b59eef3b6725d6c2
roi-louis-ix <matthewjosephbarry@outlook.com>
2024-10-01 12:12:36 -0400

    :3

--- lab6/main.c
+++ lab6/main.c
@@ -43,6 +43,7 @@ void run_lab6_part3();
 #define P1_Letter_Length    (500) //Length to display each letter in ms
 #define End_Of_Line1        (0x0F) //Value at which to change DDRAM addr to 40
 #define Running_Part        (1000) //Time to display "RUNNING PART 2" in ms
+#define Debounce            (20)
 
 
 //-----------------------------------------------------------------------------
@@ -60,11 +61,13 @@ int main(void)
 
     I2C_init();
     lcd1602_init();
+    lcd_set_display_on();
     lcd_clear();
 
     dipsw_init();
 
     run_lab6_part1();
+    run_lab6_part2();
 } /* main */
 
 void run_lab6_part1()
@@ -72,27 +75,27 @@ void run_lab6_part1()
     uint8_t loop_count = 0;
     uint8_t alphabet = 26;
     uint8_t addr = 0;
-    lcd_set_ddram_addr(addr);
-    for(loop_count; loop_count > alphabet; loop_count++)
+    char letter = 'A';
+    for(loop_count = 0; loop_count < alphabet; loop_count++)
     {
-        lcd_clear();
-        if(addr == LCD_CHAR_POSITION_16)
+        lcd_set_ddram_addr(addr);
+        lcd_write_char(letter + loop_count);
+        addr++;
+        if(addr == LCD_CHAR_POSITION_16 + 1)
         {
-            lcd_set_ddram_addr(LCD_LINE2_ADDR);
+            addr = LCD_LINE2_ADDR;
         }
-        lcd_write_char('A' + loop_count);
-        msec_delay(P1_Letter_Length);
     }
 }
 
 void run_lab6_part2()
 {
-    uint32_t top_num = quadbyte_to_ascii(1234567890);
+    uint32_t top_num = 1234567890;
     uint16_t bot_num = doublebyte_to_ascii(1234);
     uint8_t top_num2 = byte_to_ascii(123);
 
     //Begin P2
-    while(is_pb_up(PB2_IDX)) {}
+    while(is_pb_up(PB2_IDX)) {msec_delay(Debounce)}
     lcd_clear();
     lcd_write_string("Running Part 2");
     msec_delay(Running_Part);
@@ -100,20 +103,19 @@ void run_lab6_part2()
     //First two numbers displayed
     lcd_clear();
     lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_3);
-    while(is_pb_up(PB1_IDX)) {}
     lcd_write_doublebyte(top_num);
 
-    while(is_pb_up(PB1_IDX)) {}
+    while(is_pb_up(PB1_IDX)) {msec_delay(Debounce)}
     lcd_set_ddram_addr(LCD_LINE2_ADDR + LCD_CHAR_POSITION_5);
     lcd_write_doublebyte(bot_num);
 
     //Display the third number
-    while(is_pb_up(PB1_IDX)) {}
+    while(is_pb_up(PB1_IDX)) {msec_delay(Debounce)}
     lcd_clear();
     lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_6);
     lcd_write_doublebyte(top_num2);
 
-    while(is_pb_up(PB1_IDX)) {}
+    while(is_pb_up(PB1_IDX)) {msec_delay(Debounce)}
     lcd_clear();
     lcd_set_ddram_addr(LCD_LINE1_ADDR + LCD_CHAR_POSITION_3);
     lcd_write_string("Part 2 Done.");
