304c16111d53668ddca26b452190d89529c129c6
roi-louix-ix <matthewjosephbarry@outlook.com>
2024-10-29 12:43:44 -0400

    :3

--- lab9_p1/main.c
+++ lab9_p1/main.c
@@ -20,6 +20,7 @@
 //-----------------------------------------------------------------------------
 #include <stdio.h>
 
+
 //-----------------------------------------------------------------------------
 // Loads MSP launchpad board support macros and definitions
 //-----------------------------------------------------------------------------
@@ -36,7 +37,9 @@
 //-----------------------------------------------------------------------------
 // Define function prototypes used by the program
 //-----------------------------------------------------------------------------
-
+void run_lab9_p1(void);
+void config_pb1_interrupt(void);
+void config_pb2_interrupt(void);
 
 //-----------------------------------------------------------------------------
 // Define symbolic constants used by the program
@@ -44,8 +47,8 @@
 #define CONV_FACTOR (100 / 16)
 #define LOAD_VALUE          (4000)
 #define DUTY_CYCLE          (2000)
-#define UPDATE_DELAY        (100) 
-
+#define UPDATE_DELAY         (100)
+#define DEBOUNCE              (80)
 
 //-----------------------------------------------------------------------------
 // Define global variables and structures here.
@@ -62,80 +65,190 @@ int main(void)
     launchpad_gpio_init();
     keypad_init();
     dipsw_init();
+    I2C_init();
+    lcd1602_init();
+
+    led_init();
+    led_enable();
     
     motor0_init();
     motor0_pwm_init(LOAD_VALUE, 0);
+
+    
+    config_pb1_interrupt();
+    config_pb2_interrupt();
+    
+    run_lab9_p1();
 
 } /* main */
 
 void run_lab9_p1(void)
 {
+    uint8_t motor_state = 0;
     enum motor_state 
     {
-        int MOTOR_OFF1 = 0,
-        int MOTOR_CW = 1,
-        int MOTOR_OFF2 = 2,
-        int MOTOR_CCW = 3
-    }
+        MOTOR_OFF1 = 0,
+        MOTOR_CW = 1,
+        MOTOR_OFF2 = 2,
+        MOTOR_CCW = 3
+    };
 
     uint8_t duty_cycle = 0;
 
     motor_state = MOTOR_CCW;
 
+    lcd_clear();
     lcd_set_ddram_addr(LCD_CHAR_POSITION_3);
     lcd_write_string("MOTOR SPEED");
-
-    lcd_set_ddram_addr(LCD_CHAR_POSITION_7);
     
-    key_val = keypad_scan();
+    uint8_t key_val = keypad_scan();
     while(!g_PB1_pressed)
     {
         switch(motor_state)
         {
             case(MOTOR_OFF1):
-                motor0_pwm_disable();
-                lcd_write_string("0%");
-                while(!g_PB2_pressed) {}
+            
+                while(!g_PB2_pressed && !g_PB1_pressed)
+                {
+                    motor0_pwm_disable();
+                    lcd_set_ddram_addr(LCD_CHAR_POSITION_7 + LCD_LINE2_ADDR);
+                    lcd_write_string("  0");
+                    
+                    lcd_write_string("%");
+                }
+                msec_delay(DEBOUNCE);
                 motor_state = MOTOR_CW;
                 g_PB2_pressed = false;
                 break;
             
             case(MOTOR_CW):
-                while(!g_PB2_pressed) 
+                while(!g_PB2_pressed && !g_PB1_pressed) 
                 {
-                    duty_cycle = key_val * CONV_FACTOR;
+                    int8_t key_val = keypad_scan();
+                    if(key_val != NO_KEY_PRESSED)
+                    {
+                        duty_cycle = key_val * CONV_FACTOR;
+                        wait_no_key_pressed();
+                    }
                     motor0_set_pwm_dc(duty_cycle);
-                    motor0_set_pwm_count(0);
+                    led_on(LED_BAR_LD1_IDX);
+                    led_off(LED_BAR_LD2_IDX);
                     motor0_pwm_enable();
+
+                    lcd_set_ddram_addr(LCD_CHAR_POSITION_7 + LCD_LINE2_ADDR);
                     lcd_write_byte(duty_cycle);
-                    
+                    lcd_write_string("%");
                 }
+                msec_delay(DEBOUNCE);
+                key_val = 0;
+                duty_cycle = 0;
                 motor_state = MOTOR_OFF2;
                 g_PB2_pressed = false;
                 break;
 
             case(MOTOR_OFF2):
-                motor0_pwm_disable();
-                while(!g_PB2_pressed) {}
+                while(!g_PB2_pressed && !g_PB1_pressed)
+                {
+                    motor0_pwm_disable();
+                    lcd_set_ddram_addr(LCD_CHAR_POSITION_7 + LCD_LINE2_ADDR);
+                    lcd_write_string("  0");
+                    
+                    lcd_write_string("%");
+                }
+                msec_delay(DEBOUNCE);
                 motor_state = MOTOR_CCW;
                 g_PB2_pressed = false;
                 break;
 
             case(MOTOR_CCW):
-                while(!g_PB2_pressed) 
+                while(!g_PB2_pressed && !g_PB1_pressed) 
                 {
-                    duty_cycle = key_val * CONV_FACTOR;
+                    int8_t key_val = keypad_scan();
+                    if(key_val != NO_KEY_PRESSED)
+                    {
+                        
+                        duty_cycle = key_val * CONV_FACTOR;
+                    }
                     motor0_set_pwm_dc(duty_cycle);
-                    motor0_set_pwm_count(0);
+                    led_off(LED_BAR_LD1_IDX);
+                    led_on(LED_BAR_LD2_IDX);
                     motor0_pwm_enable();
+
+                    lcd_set_ddram_addr(LCD_CHAR_POSITION_7 + LCD_LINE2_ADDR);
+                    lcd_write_byte(duty_cycle);
+                    
+                    lcd_write_string("%");
                 }
-                motor_state = MOTOR_OFF2;
+                msec_delay(DEBOUNCE);
+                motor_state = MOTOR_OFF1;
+                motor0_pwm_disable();
+                
+                key_val = 0;
+                duty_cycle = 0;
                 g_PB2_pressed = false;
                 break;
 
             msec_delay(UPDATE_DELAY);
         }
     }
+    motor0_pwm_disable();
+    lcd_clear();
+    lcd_set_ddram_addr(LCD_LINE1_ADDR);
+    lcd_write_string("Program Stopped.");
+}
+
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+// This function:
+// 1) sets the polarity to the rising edge
+// 2) clears the flag
+// 3) enables masks interrupts
+// for GPIOA Port 15, or PB1.
+//  
+// INPUT PARAMETERS:
+// none
+//
+// OUTPUT PARAMETERS:
+//  none
+//
+// RETURN:
+// none
+// -----------------------------------------------------------------------------
+void config_pb1_interrupt(void)
+{
+    GPIOA->POLARITY15_0 = GPIO_POLARITY15_0_DIO15_RISE;
+    GPIOA->CPU_INT.ICLR = GPIO_CPU_INT_ICLR_DIO15_CLR;
+    GPIOA->CPU_INT.IMASK = GPIO_CPU_INT_IMASK_DIO15_SET;
+
+    NVIC_SetPriority(GPIOA_INT_IRQn, 2);
+    NVIC_EnableIRQ(GPIOA_INT_IRQn);
+}
+
+//-----------------------------------------------------------------------------
+// DESCRIPTION:
+// This function:
+// 1) sets the polarity to the rising edge
+// 2) clears the flag
+// 3) enables masks interrupts
+// for GPIOB Port 18, or PB2.
+//  
+// INPUT PARAMETERS:
+// none
+//
+// OUTPUT PARAMETERS:
+//  none
+//
+// RETURN:
+// none
+// -----------------------------------------------------------------------------
+void config_pb2_interrupt(void)
+{
+    GPIOB->POLARITY31_16 = GPIO_POLARITY31_16_DIO18_RISE;
+    GPIOB->CPU_INT.ICLR = GPIO_CPU_INT_ICLR_DIO18_CLR;
+    GPIOB->CPU_INT.IMASK = GPIO_CPU_INT_IMASK_DIO18_SET;
+
+    NVIC_SetPriority(GPIOB_INT_IRQn, 2);
+    NVIC_EnableIRQ(GPIOB_INT_IRQn);
 }
 
 //-----------------------------------------------------------------------------
